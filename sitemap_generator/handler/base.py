cbb84ce9da37c3015708086006504b2ec47ec3d3
Benjamin Webb <benjamin.miller.webb@gmail.com>
2026-05-04 14:06:15 -0400

    Revert any
    Only an issue on python 3.14

--- sitemap_generator/handler/base.py
+++ sitemap_generator/handler/base.py
@@ -30,7 +30,7 @@
 
 import logging
 from pathlib import Path
-from typing import Any
+from typing import Optional
 from xml.etree import ElementTree as ET
 
 from sitemap_generator.util import (
@@ -84,9 +84,9 @@ class FileSystemHandler:
             sitemap_location = source.canonical_sitemap_name(namespace_input_dir)
             output_path = (
                 sitemap_output_dir / "sitemap" / sitemap_location
-            ).with_suffix(".xml")
+            )
             output_path.parent.mkdir(parents=True, exist_ok=True)
-            write_tree_to_file(tree, output_path)
+            write_tree_to_file(tree, output_path.with_suffix(".xml"))
             LOGGER.info(f"Wrote {output_path} to disk")
 
         index = self.make_sitemap_index(
@@ -95,7 +95,9 @@ class FileSystemHandler:
         write_tree_to_file(index, sitemap_output_dir / "sitemap.xml")
         LOGGER.info(f"Wrote sitemap index to disk at {sitemap_output_dir}/sitemap.xml")
 
-    def make_sitemap(self, source: SitemapSourceWithMetadata) -> ET.ElementTree | Any:
+    def make_sitemap(
+            self, source: SitemapSourceWithMetadata
+        ) -> Optional[ET.ElementTree[ET.Element[str]]]:
         """
         Given a source within the filesystem tree, generate a sitemap XML
         associated with that source if it is appropriate to include,
